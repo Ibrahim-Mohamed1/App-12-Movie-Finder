ce992067f6f2ccb4a1edb15e5959bec044aac854
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-04-30 15:46:00 -0600

    movie finder

---
+++ src/DataProvider.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from "axios"
+const { Provider, Consumer } = React.createContext()
+
+class DataProvider extends Component {
+    constructor(){
+        super()
+        this.state={
+            movie: []
+        }
+    }
+
+    getMovie = (movie)=> {
+        axios.get(`https://vschool-cors.herokuapp.com?url=https://tastedive.com/api/similar?q=${movie}&k=${process.env.KEY}`).then(res => {
+            this.setState({
+                movie: res.data
+            })
+        })
+    }
+
+    render() {
+        return (
+            <Provider value={{
+                getMovie: this.getMovie,
+                ...this.state
+            }}
+            >
+                {this.props.children}
+            </Provider>
+        );
+    }
+}
+
+export default DataProvider;
+
+export function withData(C) {
+    return props => <Consumer>{value => <C {...value}{...props} />}</Consumer>
+}
